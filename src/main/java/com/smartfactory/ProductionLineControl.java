13f3377a9a9d51fb97938acf1ed2022e5e5e0681
hebercg <116598621+hebercg@users.noreply.github.com>
2024-07-23 10:28:55 +0200

    improving messages for machine operator

--- src/main/java/com/smartfactory/ProductionLineControl.java
+++ src/main/java/com/smartfactory/ProductionLineControl.java
@@ -17,8 +17,8 @@ public class ProductionLineControl {
     }
 
     public void addOrder() {
-        String orderId = JOptionPane.showInputDialog("Enter Order ID:");
-        int quantity = Integer.parseInt(JOptionPane.showInputDialog("Enter Quantity:"));
+        String orderId = JOptionPane.showInputDialog("Enter Order ID to be added to production:");
+        int quantity = Integer.parseInt(JOptionPane.showInputDialog("Enter quantity of boxes to be packed :"));
 
         com.smartfactory.LaminatePackingProto.ProductionLineControlRequest request = com.smartfactory.LaminatePackingProto.ProductionLineControlRequest.newBuilder()
                 .setOrderID(orderId)
@@ -30,7 +30,7 @@ public class ProductionLineControl {
         JOptionPane.showMessageDialog(null, "Status: " + response.getStatus() + "\nMessage: " + response.getMessage());
     }
     public void startProduction() {
-        String orderId = JOptionPane.showInputDialog("Enter Order ID:");
+        String orderId = JOptionPane.showInputDialog("Enter Order ID to start the production:");
 
         com.smartfactory.LaminatePackingProto.StartProductionRequest request = com.smartfactory.LaminatePackingProto.StartProductionRequest.newBuilder()
                 .setOrderID(orderId)
@@ -42,8 +42,8 @@ public class ProductionLineControl {
     }
 
     public void stopProduction() {
-        String orderId = JOptionPane.showInputDialog("Enter Order ID:");
-        String reason = JOptionPane.showInputDialog("Enter Reason for Stopping:");
+        String orderId = JOptionPane.showInputDialog("Enter Order ID to stop the production:");
+        String reason = JOptionPane.showInputDialog("Enter Reason for Stopping : " + "1: Missing material, 2: Wrong order");
 
         com.smartfactory.LaminatePackingProto.StopProductionRequest request = com.smartfactory.LaminatePackingProto.StopProductionRequest.newBuilder()
                 .setOrderID(orderId)
@@ -55,9 +55,9 @@ public class ProductionLineControl {
         JOptionPane.showMessageDialog(null, "Status: " + response.getStatus() + "\nMessage: " + response.getMessage());
     }
     public void updateOrder() {
-        String orderId = JOptionPane.showInputDialog("Enter Order ID:");
-        int quantity = Integer.parseInt(JOptionPane.showInputDialog("Enter Quantity:"));
-        String description = JOptionPane.showInputDialog("Enter Description:");
+        String orderId = JOptionPane.showInputDialog("Enter Order ID to be updated:");
+        int quantity = Integer.parseInt(JOptionPane.showInputDialog("Enter Quantity to be added or removed from order:"));
+        String description = JOptionPane.showInputDialog("Enter reason for update the order: " + "ex.: Increase of order, Wrong colour...");
 
         LaminatePackingProto.UpdateOrderRequest request = LaminatePackingProto.UpdateOrderRequest.newBuilder()
                 .setOrderID(orderId)
@@ -70,10 +70,10 @@ public class ProductionLineControl {
         JOptionPane.showMessageDialog(null, "Status: " + response.getStatus() + "\nMessage: " + response.getMessage());
     }
 
-
+//production stage that has surveillance, multiple stages of the packing line.
     public void productionSurveillance() {
-        String stage = JOptionPane.showInputDialog("Enter Production Stage:");
-        String inspectionResult = JOptionPane.showInputDialog("Enter Inspection Result:");
+        String stage = JOptionPane.showInputDialog("Enter Production Stage: " + "1: box entry, 2: laminate entry, 3: plastic cover, 4 packing exit ");
+        String inspectionResult = JOptionPane.showInputDialog("Enter Inspection Result: " + "passed or failed ");
 
         LaminatePackingProto.ProductionSurveillanceRequest request = LaminatePackingProto.ProductionSurveillanceRequest.newBuilder()
                 .setStage(stage)
@@ -94,8 +94,8 @@ public class ProductionLineControl {
     }
 
     public void packagingLineJamAlerts() {
-        String sensorID = JOptionPane.showInputDialog("Enter Sensor ID:");
-        String timestamp = JOptionPane.showInputDialog("Enter Timestamp:");
+        String sensorID = JOptionPane.showInputDialog("Enter Sensor ID: " + "1: box feed, 2: Laminate feed, 3: Plastic cover");
+        String timestamp = JOptionPane.showInputDialog("Enter Timestamp: " + "ex.: 23/07/2024" );
 
         LaminatePackingProto.PackagingLineJamAlertsRequest request = LaminatePackingProto.PackagingLineJamAlertsRequest.newBuilder()
                 .setSensorID(sensorID)
@@ -109,7 +109,7 @@ public class ProductionLineControl {
 
     public void productionScheduling() {
         String orderId = JOptionPane.showInputDialog("Enter Order ID:");
-        String scheduledTime = JOptionPane.showInputDialog("Enter Scheduled Time:");
+        String scheduledTime = JOptionPane.showInputDialog("Enter Scheduled Time: " + " ex.: 12:04:00");
 
         LaminatePackingProto.ProductionSchedulingRequest request = LaminatePackingProto.ProductionSchedulingRequest.newBuilder()
                 .setOrderID(orderId)
